30dac81b7baa3da4eb064d76b1e125a8b81b2ad5
win860812 <36957387+win860812@users.noreply.github.com>
2020-07-09 22:39:28 +0800

    init ver

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Test1(View view) {
+        finish();
+    }
+}
